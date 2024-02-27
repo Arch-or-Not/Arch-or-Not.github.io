22f60ab13f3b0a2ad7beb4d940662c97d226e698
Baran Acar <83635600+Arch-or-Not@users.noreply.github.com>
2024-02-27 23:25:36 +0300

    Update send_email.php

--- send_email.php
+++ send_email.php
@@ -1,32 +1,25 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
-// POST isteğini kontrol et
-if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-  http_response_code(405);
-  exit();
-}
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  // Form verilerini al
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $name = $_POST['name'];
   $email = $_POST['email'];
   $message = $_POST['message'];
 
-  // E-posta bilgileri
   $to = "acarb0040@gmail.com";
   $subject = "İletişim Formundan Yeni Mesaj";
   $body = "Ad: $name\nE-posta: $email\n\nMesaj:\n$message";
 
-  // E-posta gönderme
   if (mail($to, $subject, $body)) {
-    http_response_code(200); // Başarılı durum kodu
+    http_response_code(200);
+    echo "E-posta başarıyla gönderildi.";
   } else {
-    http_response_code(500); // Hata durum kodu
+    http_response_code(500);
+    echo "E-posta gönderme hatası.";
   }
 } else {
-  http_response_code(405); // Method Not Allowed durum kodu
+  http_response_code(405);
+  echo "Hatalı istek.";
 }
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
 ?>
