884ed2cab3fdb6dee3e7c74b2953ba6ecd1c01f3
Baran Acar <83635600+Arch-or-Not@users.noreply.github.com>
2024-02-27 23:08:30 +0300

    Update send_email.php

--- send_email.php
+++ send_email.php
@@ -1,27 +1,29 @@
 <?php
+
+// POST isteğini kontrol et
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+  http_response_code(405);
+  exit();
+}
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $data = json_decode(file_get_contents("php://input"));
+  // Form verilerini al
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $message = $_POST['message'];
 
-  $name = $data->name;
-  $email = $data->email;
-  $message = $data->message;
-
+  // E-posta bilgileri
   $to = "acarb0040@gmail.com";
-  $subject = "KOSBİ FPV Web Sitesinden Yeni Bir Mesajınız Var";
-  $body = "Adı: $name\n";
-  $body .= "E-posta: $email\n";
-  $body .= "Mesaj:\n$message";
+  $subject = "İletişim Formundan Yeni Mesaj";
+  $body = "Ad: $name\nE-posta: $email\n\nMesaj:\n$message";
 
-  $headers = "From: acarb0040@gmail.com" . "\r\n" .
-             "Reply-To: $email" . "\r\n" .
-             "X-Mailer: PHP/" . phpversion();
-
-  if (mail($to, $subject, $body, $headers)) {
-    http_response_code(200);
+  // E-posta gönderme
+  if (mail($to, $subject, $body)) {
+    http_response_code(200); // Başarılı durum kodu
   } else {
-    http_response_code(500);
+    http_response_code(500); // Hata durum kodu
   }
 } else {
-  http_response_code(405);
+  http_response_code(405); // Method Not Allowed durum kodu
 }
 ?>
